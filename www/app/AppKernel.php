119a0c2659f1e4b8b01e791a14a21ecc608a94e1
davidgiamporcaro@gmail.com <davidgiamporcaro@gmail.com@8140dc33-c1fe-cd7c-c8e0-e489d1059ce4>
2013-03-21 16:30:26 +0000

    Adding SurveyBundle

--- www/app/AppKernel.php
+++ www/app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
 
             new FOS\UserBundle\FOSUserBundle(),
             new Sondage\UserBundle\UserBundle(),
+            new Sondage\SurveyBundle\SondageSurveyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
