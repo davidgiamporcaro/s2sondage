7d5397061e22bbd6ccb2d62edc5c743264607614
davidgiamporcaro@gmail.com <davidgiamporcaro@gmail.com@8140dc33-c1fe-cd7c-c8e0-e489d1059ce4>
2013-11-04 17:15:52 +0000

    Configure homepage carousel

--- www/src/Sondage/SettingsBundle/Controller/DefaultController.php
+++ www/src/Sondage/SettingsBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace Sondage\SettingsBundle\Controller;
 
+use Sondage\SettingsBundle\SondageSettingsBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sondage\SettingsBundle\Form\Type\SettingsType;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sondage\SettingsBundle\Entity\Settings;
+use Sondage\SettingsBundle\Entity\Document;
+
 
 class DefaultController extends Controller
 {
@@ -12,15 +16,32 @@ class DefaultController extends Controller
     {
         if( $this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ) {
             $em = $this->getDoctrine()->getManager();
-            $qb = $em->getRepository('SondageSettingsBundle:Settings')->createQueryBuilder('s');
-            $settings = $qb->select('s');
-            if (!$settings) {
-                throw $this->createNotFoundException('Unable to find any settings.');
+            $settings = new Settings();
+            $form = $this->createForm(new SettingsType, $settings);
+
+            $request = $this->getRequest();
+            if ('POST' == $request->getMethod()) {
+                $form->bind($request);
+                if ($form->isValid()) {
+                    $document = $settings->getDocuments();
+                    $em->persist($settings);
+                    $em->persist($document);
+                    $em->flush();
+
+                    $this->get('session')->setFlash('success', 'New settings was saved!');
+
+                }
+                else {
+                    $this->get('session')->setFlash('error', 'The settings was not saved!');
+                }
             }
-            $form = $this->createForm(new SettingsType());
+
             return $this->render('SondageSettingsBundle:Default:configure.html.twig', array(
                 'form' => $form->createView(),
             ));
+        }
+        else {
+            throw new AccessDeniedException();
         }
     }
 }
--- www/src/Sondage/SettingsBundle/Form/Type/DocumentType.php
+++ www/src/Sondage/SettingsBundle/Form/Type/DocumentType.php
@@ -6,25 +6,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class SettingsType extends AbstractType
+class DocumentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('documents', new DocumentType())
+            ->add('file', 'file')
         ;
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Sondage\SettingsBundle\Entity\Settings'
+            'data_class' => 'Sondage\SettingsBundle\Entity\Document'
         ));
     }
 
     public function getName()
     {
-        return 'sondage_settingsbundle_settingstype';
+        return 'sondage_settingsbundle_documenttype';
     }
 }
